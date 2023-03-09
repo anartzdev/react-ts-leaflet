569f7ae456cc72818342d7d27efed24953ac743c
anartzdev <105705996+anartzdev@users.noreply.github.com>
2023-03-09 13:34:09 +0100

    1 file modified

--- Marker/Functions.tsx
+++ Marker/Functions.tsx
@@ -26,12 +26,9 @@ export function addMarkers(
       markerElement
         .bindPopup(markers[0].popup.content)
         .on('click', function () {
-          const moveCamera =
-            markerElement.getLatLng().lat < map.getCenter().lat
-              ? -0.0025
-              : 0.0025;
-          map.panTo(
-            new LatLng(map.getCenter().lat + moveCamera, map.getCenter().lng)
+          map.flyTo(
+            [markerElement.getLatLng().lat, markerElement.getLatLng().lng],
+            map.getZoom()
           );
         });
     return;
